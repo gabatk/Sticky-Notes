4e36eba84464d58379ea013c0b98e2a9daba2244
gabatk <gabriela.trybuszek@gmail.com>
2023-01-09 18:27:24 +0100

    JavaScript functions in progress.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const addBtn = document.querySelector('.menu__button-add');
-const deleteBtn = document.querySelector('.menu__button-delete-all');
+const deleteAllBtn = document.querySelector('.menu__button-delete-all');
 const saveBtn = document.querySelector('.note-panel__save');
 const cancelBtn = document.querySelector('.note-panel__cancel');
 const deleteBtns = document.getElementsByClassName('note__header-delete');
@@ -8,3 +8,20 @@ const notePanel = document.querySelector('.note-panel');
 const panelCategory = document.querySelector('#category');
 const panelText = document.querySelector('#text');
 const panelError = document.querySelector('.note-panel__error');
+
+let selectedValue;
+let cardID = 0;
+
+const showPanel = () => {
+	notePanel.style.display = 'flex';
+};
+
+const closePanel = () => {
+	notePanel.style.display = 'none';
+	panelError.style.visibility = 'hidden;';
+	panelCategory.selectedIndex = 0;
+	panelText.value = '';
+};
+
+addBtn.addEventListener('click', showPanel);
+cancelBtn.addEventListener('click', closePanel);
